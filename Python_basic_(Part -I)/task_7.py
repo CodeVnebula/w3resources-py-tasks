b8004675b68e039f9adc682b0cd69fb3661bd751
CodeVnebula <chkhikvadzem471@gmail.com>
2024-03-27 23:50:36 +0400

    Added new tasks and resources

---
+++ Python_basic_(Part -I)/task_7.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program that accepts a filename from the user and prints the extension of the file.
+Sample filename : abc.java
+Output : java
+"""
+
+file_name = input('Enter full filename: ')
+extension = file_name.split('.')[-1]
+print("Extension: " + extension)
