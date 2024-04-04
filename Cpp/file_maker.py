62e1317364031cb2e6c61eb3cbe384e08e67c857
CodeVnebula <chkhikvadzem471@gmail.com>
2024-04-04 14:41:31 +0400

    Added file maker-removers

---
+++ Cpp/file_maker.py
@@ -0,0 +1,46 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.w3resource.com/cpp-exercises/basic/index.php"
+# Link can be changed for different task pages
+
+path = "Cpp/"
+folder_name = path + "C++Basic"
+
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+start = int(input("File index to srtart: "))
+end = int(input("Amount of files to create: "))
+
+response = requests.get(URL)
+
+soup = BeautifulSoup(response.content, 'lxml')
+
+p_tags_with_strong = soup.select('p > strong')  # To find every p tags which contain strong tag
+p_tags_with_strong = p_tags_with_strong[1:-2]
+
+for i in range(start, start + end):
+    for strong_tag in p_tags_with_strong:
+
+        parent_p_tag = strong_tag.parent
+        
+        a_tag = parent_p_tag.find('a')
+        
+        if a_tag:
+            link = "Link: " + "https://www.w3resource.com/cpp-exercises/" + a_tag['href']
+        else:
+            link = "\nLink: " + "https://www.w3resource.com/cpp-exercises/basic/cpp-basic-exercise-" + str(i) + ".php"
+
+        result_string = parent_p_tag.text.strip().replace('Click me to see the sample solution', '')
+
+        task_index = result_string[:3].replace(' ', '').replace('.', '')
+     
+        if int(task_index) == i:
+            file_name = os.path.join(folder_name, "task{}.cpp".format(i))
+            with open(file_name, "w") as file:
+                file.write(f'/*\n{result_string}\n{link}\n*/\n\n// Solution\n\n')
+
+            print("'{}' has been created successfully.".format(file_name))
+            break
---
+++ Cpp/file_remover.py
@@ -0,0 +1,20 @@
+import os
+
+def delete_file(file_name: str, file_index: int, file_amount: int):
+    try:
+        for i in range(file_index, file_index - file_amount, -1):
+            file_path = folder_name + "/" + file_name.format(index = i)
+            os.remove(file_path)
+            print(f'{file_name.format(index = i)} has been removed successfully!') 
+    except OSError as e:
+        print(f"Error deleting {file_name}: {e}")
+
+path = "Cpp/"
+folder_name = path + "C++Basic"
+file_name = "task{index}.cpp"
+
+
+file_index = int(input("Current index of file: "))
+file_amount = int(input("Amount of files to remove: "))
+
+delete_file(file_name, file_index, file_amount)
